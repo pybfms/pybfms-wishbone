aebaa007df5c962f548dd330256546aa6b4d9643
Matthew Ballance <matt.ballance@gmail.com>
2020-11-26 12:54:24 -0800

    initial support
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/wishbone_bfms/hdl/wb_initiator_bfm.v
+++ src/wishbone_bfms/hdl/wb_initiator_bfm.v
@@ -4,8 +4,8 @@
  ****************************************************************************/
 
 module wb_initiator_bfm #(
-		ADDR_WIDTH = 32,
-		DATA_WIDTH = 32
+		parameter ADDR_WIDTH = 32,
+		parameter DATA_WIDTH = 32
 		) (
 		input							clock,
 		input							reset,
@@ -18,12 +18,15 @@ module wb_initiator_bfm #(
 		input							ack_i,
 		input[DATA_WIDTH-1:0]			dat_i
 		);
-	reg						stb_v;
-	reg						cyc_v;
-	reg						we_v;
-	reg[(DATA_WIDTH/8)-1:0]	sel_v;
-	reg[DATA_WIDTH-1:0]		dat_v;
-	reg[ADDR_WIDTH-1:0]		adr_v;
+	reg						stb_v = 0;
+	reg						cyc_v = 0;
+	reg						we_v = 0;
+	reg[(DATA_WIDTH/8)-1:0]	sel_v = 0;
+	reg[DATA_WIDTH-1:0]		dat_v = 0;
+	reg[ADDR_WIDTH-1:0]		adr_v = 0;
+	
+	reg						in_reset = 0;
+	reg[3:0]				post_reset_delay = 0;
 	
 	always @(posedge clock) begin
 		if (reset) begin
@@ -33,7 +36,17 @@ module wb_initiator_bfm #(
 			sel_o <= 0;
 			dat_o <= 0;
 			adr_o <= 0;
+			in_reset <= 1;
+			post_reset_delay <= 0;
 		end else begin
+			if (in_reset) begin
+				if (post_reset_delay == 4'hf) begin
+					_reset();
+					in_reset <= 0;
+				end else begin
+					post_reset_delay <= post_reset_delay + 1;
+				end
+			end
 			stb_o <= stb_v;
 			cyc_o <= cyc_v;
 			we_o <= we_v;
@@ -49,25 +62,35 @@ module wb_initiator_bfm #(
 				sel_v = 0;
 				dat_v = 0;
 				adr_v = 0;
+				stb_o <= 0;
+				cyc_o <= 0;
+				we_o <= 0;
 			end
 		end
 	end
 	
 	task _access_req(
-		reg[63:0]			adr,
-		reg[63:0]			dat,
-		reg[7:0]			we,
-		reg[7:0]			sel);
+		input reg[63:0]			adr,
+		input reg[63:0]			dat,
+		input reg[7:0]			we,
+		input reg[7:0]			sel);
+	begin
+		$display("--> _access_req");
 		stb_v = 1;
 		cyc_v = 1;
 		we_v = we;
 		sel_v = sel;
 		dat_v = dat;
 		adr_v = adr;
+		$display("<-- _access_req");
+	end
 	endtask
 
-	task init();
+	task init;
+	begin
+		$display("wb_initiator_bfm: %m");
 		_set_parameters(ADDR_WIDTH, DATA_WIDTH);
+	end
 	endtask
 
 	// Auto-generated code to implement the BFM API
--- src/wishbone_bfms/wb_initiator_bfm.py
+++ src/wishbone_bfms/wb_initiator_bfm.py
@@ -7,8 +7,8 @@ Created on Nov 21, 2020
 import pybfms
 
 @pybfms.bfm(hdl={
-    pybfms.BfmType.Verilog : pybfms.bfm_hdnl_path(__file__, "hdl/wb_initiator_bfm.v"),
-    pybfms.BfmType.SystemVerilog : pybfms.bfm_hdnl_path(__file__, "hdl/wb_initiator_bfm.v"),
+    pybfms.BfmType.Verilog : pybfms.bfm_hdl_path(__file__, "hdl/wb_initiator_bfm.v"),
+    pybfms.BfmType.SystemVerilog : pybfms.bfm_hdl_path(__file__, "hdl/wb_initiator_bfm.v"),
     }, has_init=True)
 class WbInitiatorBfm():
 
@@ -18,9 +18,16 @@ class WbInitiatorBfm():
         self.ack_ev = pybfms.event()
         self.addr_width = 0
         self.data_width = 0
+        self.reset_ev = pybfms.event()
+        self.is_reset = False
         
     async def write(self, adr, dat, sel):
         await self.busy.acquire()
+
+        if not self.is_reset:
+            await self.reset_ev.wait()
+            self.reset_ev.clear()
+
         self._access_req(adr, dat, 1, sel)
         
         await self.ack_ev.wait()
@@ -28,16 +35,41 @@ class WbInitiatorBfm():
         
         self.busy.release()
         
-    @pybfms.import_task(pybfms.uint64,pybfms.uint64,pybfms.uint8,pybfms.uint8)
+    async def read(self, adr):
+        await self.busy.acquire()
+        
+        if not self.is_reset:
+            await self.reset_ev.wait()
+            self.reset_ev.clear()
+            
+        self._access_req(adr, 0, 0, 0)
+        
+        await self.ack_ev.wait()
+        self.ack_ev.clear()
+        
+        self.busy.release()
+        
+        return self.dat_i
+        
+    @pybfms.import_task(pybfms.uint64_t,pybfms.uint64_t,pybfms.uint8_t,pybfms.uint8_t)
     def _access_req(self, adr, dat, we, sel):
         pass
     
-    @pybfms.export_task(pybfms.uint64)
+    @pybfms.export_task(pybfms.uint64_t)
     def _access_ack(self, dat_i):
+        print("--> WbInitiatorBfm::_access_ack")
         self.dat_i = dat_i
         self.ack_ev.set()
+        print("<-- WbInitiatorBfm::_access_ack")
         
-    @pybfms.export_task(pybfms.uint32,pybfms.uint32)
+    @pybfms.export_task(pybfms.uint32_t,pybfms.uint32_t)
     def _set_parameters(self, addr_width, data_width):
         self.addr_width = addr_width
         self.data_width = data_width
+        
+    @pybfms.export_task()
+    def _reset(self):
+        print("--> _reset")
+        self.is_reset = True
+        self.reset_ev.set()
+        print("<-- _reset")
